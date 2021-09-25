6c89318c3137ce4ed416aceaa4057373252ebdb8
LiH <617981423@qq.com>
2021-09-25 22:43:51 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-select
+    v-model="selected"
+    mode="multiple"
+    style="width: 100%"
+    placeholder="Please select"
+  >
+    <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+      {{ (i + 9).toString(36) + i }}
+    </a-select-option>
+  </a-select>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selected: [],
+    };
+  },
+  methods: {
+    handleChange(value) {
+      console.log(`selected ${value}`);
+    },
+  },
+};
+</script>
